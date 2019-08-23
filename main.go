4409b02673c0367deb01d8905c6be0c876bf749d
Tam Ngo <minhtam0412@gmail.com>
2019-08-23 10:17:33 +0700

    chuẩn hoá code

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
 	"github.com/auth0/go-jwt-middleware"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gin-gonic/gin"
 	"github.com/labstack/echo"
 	"jokeapp/controller"
 	"jokeapp/middleware"
 	"net/http"
-	"os"
 )
 
 //https://dev.to/codehakase/building-a-web-app-with-go-gin-and-react-5ke
@@ -45,53 +39,4 @@ func main() {
 	e.GET("/jokes", h.JokeHandler, middleware.IsLoggedIn)
 	e.POST("/jokes/like/:jokeID", h.LikeJoke)
 	e.Logger.Fatal(e.Start(":1323"))
-
-	// Our API will consit of just two routes
-	// /jokes - which will retrieve a list of jokes a user can see
-	// /jokes/like/:jokeID - which will capture likes sent to a particular joke
-}
-
-func getPemCert(token *jwt.Token) (string, error) {
-	cert := ""
-	resp, err := http.Get(os.Getenv("AUTH0_DOMAIN") + ".well-known/jwks.json")
-	if err != nil {
-		return cert, err
-	}
-	defer resp.Body.Close()
-
-	var jwks = Jwks{}
-	err = json.NewDecoder(resp.Body).Decode(&jwks)
-
-	if err != nil {
-		return cert, err
-	}
-
-	x5c := jwks.Keys[0].X5c
-	for k, v := range x5c {
-		if token.Header["kid"] == jwks.Keys[k].Kid {
-			cert = "-----BEGIN CERTIFICATE-----\n" + v + "\n-----END CERTIFICATE-----"
-		}
-	}
-
-	if cert == "" {
-		return cert, errors.New("unable to find appropriate key.")
-	}
-
-	return cert, nil
-}
-
-// authMiddleware intercepts the requests, and check for a valid jwt token
-func authMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// Get the client secret key
-		err := jwtMiddleWare.CheckJWT(c.Writer, c.Request)
-		if err != nil {
-			// Token not found
-			fmt.Println(err)
-			c.Abort()
-			c.Writer.WriteHeader(http.StatusUnauthorized)
-			c.Writer.Write([]byte("Unauthorized"))
-			return
-		}
-	}
 }
